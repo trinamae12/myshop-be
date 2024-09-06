b66425c02281171f603a3230562e2080bcceb502
Trina Mae Abarquez <trinamaeabarquez@Trinas-MacBook-Air.local>
2024-09-06 17:33:06 +0800

    Add product and product category model, controller and migration

---
+++ src/database/migrations/2024_09_06_090707_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('product_categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //$table->dropForeign('products_category_id_foreign');
+        //$table->dropIndex('lists_user_id_index');
+        //$table->dropColumn('user_id');
+        Schema::dropIfExists('products');
+    }
+};
